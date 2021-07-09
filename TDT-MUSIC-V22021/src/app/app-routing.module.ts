b81c13b1f3348f034bf6e38b08cb660708e2096e
kibo <minhtuanvt89@gmail.com>
2021-07-09 08:32:44 +0700

    song list & CRUD

--- TDT-MUSIC-V22021/src/app/app-routing.module.ts
+++ TDT-MUSIC-V22021/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SongComponent} from "../../../../Angular-Project-TDT-Music-2021/my-project/src/app/component/song/song.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'component/song',
+    component: SongComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TDT-MUSIC-V22021/src/app/app.module.ts
+++ TDT-MUSIC-V22021/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongComponent } from './component/song/song.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {NavbarComponent} from "./component/layout/navbar/navbar.component";
+import {MenuComponent} from "./component/layout/menu/menu.component";
+import {FooterComponent} from "./component/layout/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongComponent,
+    NavbarComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
